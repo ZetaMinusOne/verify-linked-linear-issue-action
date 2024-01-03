4ad161669c02f253792cb54b20b9c9a26226741a
Javier Muniz <javier.muniz@zetaminusone.com>
2024-01-03 12:09:39 -0400

    implement linear action code

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -11,16 +11,68 @@ import * as main from '../src/main'
 
 // Mock the action's main function
 const runMock = jest.spyOn(main, 'run')
+let createComment = jest.fn().mockImplementation();
 
-// Other utilities
-const timeRegex = /^\d{2}:\d{2}:\d{2}/
+let mockData: any[] = [];
+const validData = [
+  {
+    performed_via_github_app: {
+      slug: 'linear'
+    },
+    body: 'href="https://linear.app/1234"'
+  }
+]
+
+const invalidData = [
+  {
+    performed_via_github_app: {
+      slug: 'github'
+    },
+    body: 'href="https://github.com/1234"'
+  }
+]
+
+jest.mock('@actions/github', () => {
+  const originalModule = jest.requireActual('@actions/github');
+
+  //Mock the default export and named export 'foo'
+  return {
+    __esModule: true,
+    ...originalModule,
+    getOctokit: jest.fn().mockImplementation(() => {
+      return {
+        rest: {
+          issues: {
+            listComments: jest.fn().mockImplementation(() => {
+              return {
+                data: mockData
+              }
+            }),
+            createComment,
+          }
+        }
+      } as any
+    }),
+    context: {
+      repo: {
+        owner: 'test',
+        repo: 'test'
+      },
+      payload: {
+        pull_request: {
+          number: 1234
+        }
+      }
+    }
+  };
+});
 
 // Mock the GitHub Actions core library
 let debugMock: jest.SpyInstance
 let errorMock: jest.SpyInstance
-let getInputMock: jest.SpyInstance
 let setFailedMock: jest.SpyInstance
-let setOutputMock: jest.SpyInstance
+let noticeMock: jest.SpyInstance
+
 
 describe('action', () => {
   beforeEach(() => {
@@ -28,62 +80,52 @@ describe('action', () => {
 
     debugMock = jest.spyOn(core, 'debug').mockImplementation()
     errorMock = jest.spyOn(core, 'error').mockImplementation()
-    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
     setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
-    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+    noticeMock = jest.spyOn(core, 'notice').mockImplementation()
   })
 
-  it('sets the time output', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return '500'
-        default:
-          return ''
-      }
-    })
-
+  it('successfully finds the linear ticket', async () => {
+    mockData = validData;
     await main.run()
     expect(runMock).toHaveReturned()
 
     // Verify that all of the core library functions were called correctly
-    expect(debugMock).toHaveBeenNthCalledWith(1, 'Waiting 500 milliseconds ...')
-    expect(debugMock).toHaveBeenNthCalledWith(
-      2,
-      expect.stringMatching(timeRegex)
-    )
-    expect(debugMock).toHaveBeenNthCalledWith(
-      3,
-      expect.stringMatching(timeRegex)
-    )
-    expect(setOutputMock).toHaveBeenNthCalledWith(
-      1,
-      'time',
-      expect.stringMatching(timeRegex)
-    )
+    expect(debugMock).toHaveBeenNthCalledWith(1, 'Searching for Linear ticket link ...')
+    expect(noticeMock).toHaveBeenCalledWith('Found Linear ticket.')
     expect(errorMock).not.toHaveBeenCalled()
   })
+  
 
   it('sets a failed status', async () => {
-    // Set the action's inputs as return values from core.getInput()
-    getInputMock.mockImplementation((name: string): string => {
-      switch (name) {
-        case 'milliseconds':
-          return 'this is not a number'
-        default:
-          return ''
-      }
+    mockData = invalidData;
+    await main.run()
+    expect(runMock).toHaveReturned()
+
+    expect(createComment).toHaveBeenCalledWith({
+      issue_number: 1234,
+      owner: 'test',
+      repo: 'test',
+      body: `No Linear ticket found for this pull request. Please link an issue in Linear by mentioning the ticket.`
     })
 
+    // Verify that all of the core library functions were called correctly
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      'No Linear ticket found.'
+    )
+  })
+
+  it('requires a pull request', async() => {
+    mockData = validData;
+    const context = require('@actions/github').context;
+    context.payload.pull_request = undefined;
     await main.run()
     expect(runMock).toHaveReturned()
 
     // Verify that all of the core library functions were called correctly
     expect(setFailedMock).toHaveBeenNthCalledWith(
       1,
-      'milliseconds not a number'
+      'No pull request number found in context, exiting.'
     )
-    expect(errorMock).not.toHaveBeenCalled()
   })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+import * as github from '@actions/github'
+
 
 /**
  * The main function for the action.
@@ -7,18 +8,35 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
+    const token = process.env.GITHUB_TOKEN as string;
+    const octokit = github.getOctokit(token);
+    const context = github.context;
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    core.debug(`Searching for Linear ticket link ...`)
+    
+    if(!context.payload.pull_request?.number) {
+      throw new Error('No pull request number found in context, exiting.')
+    }
+    
+    const comments = await octokit.rest.issues.listComments({
+      issue_number: context.payload.pull_request?.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo
+    });
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    const linearComment = comments.data.find(comment => comment.performed_via_github_app?.slug === 'linear' && comment.body?.includes('href="https://linear.app/'));
+    if(linearComment) {
+      core.notice(`Found Linear ticket.`);
+    } else {
+      await octokit.rest.issues.createComment({
+        issue_number: context.payload.pull_request?.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: `No Linear ticket found for this pull request. Please link an issue in Linear by mentioning the ticket.`
+      });
+      core.error(`No Linear ticket found.`);
+      core.setFailed('No Linear ticket found.');
+    }
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
